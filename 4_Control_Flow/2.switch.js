5eecb292c9ac1751fa687e83b886ec2bf41d46b1
VAAN <vaan.origin@gmail.com>
2024-08-21 15:56:13 +0530

    switch statement completed

---
+++ 4_Control_Flow/2.switch.js
@@ -0,0 +1,41 @@
+// switch statement 
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// Example :
+
+const day = "mon"  // key = value
+switch (day) {
+    case "mon":
+        console.log("Monday")
+        // break;
+    case "tue":
+        console.log("Tuesday")
+        break;
+    case "wed":
+        console.log("Wednesday")
+        break;
+    case "thu":
+        console.log("Thursday")
+        // break;
+    case "fri":
+        console.log("Friday")
+        break;
+    case "sat":
+        console.log("Saturday")
+        break;
+    case "sat":
+        console.log("Sunday")
+        break;
+
+    default:
+        console.log("invalid input")
+        break;
+}
