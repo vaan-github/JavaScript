3613dd17c77a8ebb34d941efb99421e48df7f9a7
VAAN <vaan.origin@gmail.com>
2024-08-21 18:05:31 +0530

    while loop completed

---
+++ 5_Iterations/2.while.js
@@ -0,0 +1,37 @@
+// while loop structure
+
+
+// Initialization
+// while (condition) {
+    // code 
+    // Operation ==> prevent the loop to go infinite
+// }
+
+// while loop with array
+
+const myHeroes = ["Batman", "Superman", "DeadPool", "Wolverine"] // array
+
+let index = 0  // initialization
+while (index < myHeroes.length) {
+    // console.log(myHeroes[index])
+    index++ // index = index + 1  // Operation
+}
+
+
+// do-while loop structure
+
+// // initialization
+// do {
+//     // code 
+//     //operation
+    
+// } while (condition);
+
+let score = 12;
+
+do {
+    console.log(score)  // execute the code then check the condition
+    score++
+
+} while (score <=10);
+
