3fc54e40169f070212346e40ab59bc76358b213b
VAAN <vaan.origin@gmail.com>
2024-08-24 15:21:25 +0530

    some comments

--- 7_Projects/1.colorChanger/script.js
+++ 7_Projects/1.colorChanger/script.js
@@ -6,9 +6,9 @@ const body = document.querySelector('body')
 buttons.forEach((eachButton)=>{
     console.log(eachButton)
     eachButton.addEventListener('click', function(e){
-        // console.log(e)
+        // console.log(e) // e is a pointerEvent
         // console.log(e.target)
-        document.body.style.backgroundColor = e.target.id
+        document.body.style.backgroundColor = e.target.id // This code is my thinking.
 
     })
 
